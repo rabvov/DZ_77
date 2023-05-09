1452d5fe954fe50e75b88e1740f6eb973fd85d53
rak15 <rak1529916@mail.ru>
2023-05-09 16:42:15 +0300

    1.1

--- src/DZ_77.java
+++ src/DZ_77.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class DZ_77 {
+import java.util.Date;
+
+public class DZ_77 {
+    public static void main(String[] args) {
+        MyTread myTread = new MyTread();
+        myTread.start();
+    }
 }
--- src/MyTread.java
+++ src/MyTread.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class MyTread {
+import java.util.Date;
+
+public class MyTread extends Thread{
+    public void run(){
+        Date date = new Date();
+        System.out.println(date);
+    }
 }
